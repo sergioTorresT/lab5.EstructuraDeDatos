81a4dce5341e7a8327ea56e211232e93812427d7
Your Name <you@example.com>
2026-05-17 01:55:05 +0000

    new try

--- graph.c
+++ graph.c
@@ -91,7 +91,7 @@ List* getAdjacentLabels(Graph* g, const char* label) {
     List *nuevaLista = list_create();
     Edge *edge = (Edge *)list_first(listaEdge);
     while(edge != NULL){
-        list_pushFront(nuevaLista, (void)edge -> target);
+        list_pushFront(nuevaLista, (void *)edge -> target);
     }
 
     return nuevaLista; 
